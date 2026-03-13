3a59f60f316f6a813465c3e3c6b9ba0e96c808f0
kiranpatil22 <kiranpatiluniverse@gmail.com>
2026-03-13 18:56:54 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,23 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  type        = string
+  default     = "us-west-2"
 }
 
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+  description = "AMI ID used for launching the EC2 instance"
+  type        = string
   default     = "ami-085f9c64a9b75eed5"
 }
 
 variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  default     = "t2.medium"
+  description = "EC2 instance type for compute resources"
+  type        = string
+  default     = "c7i-flex.large"
 }
 
-variable "my_enviroment" {
-  description = "Instance type for the EC2 instance"
+variable "my_environment" {
+  description = "Deployment environment (dev, stage, prod)"
+  type        = string
   default     = "dev"
 }
